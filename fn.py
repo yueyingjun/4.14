1f3f527fbfbab69fee513d34c8409811fb967a09
yueyingjuna <1046396106@qq.com>
2018-04-14 17:33:22 +0800

    select

--- fn.py
+++ fn.py
@@ -1,6 +1,8 @@
 asdasdas= [32, 12, 20, 16, 3, 1, 2];
+
 # 冒泡排序
 # 插入排序
+
 def sort(arr,type=1):
     for i in range(len(arr)):
         for j in range(i+1,len(arr)):
@@ -17,4 +19,25 @@ def sort(arr,type=1):
 
     return arr
 
-print(sort(asdasdas,4))
+arr1=[213,13,43,12,23]
+def insert (arr):
+    for curr in range(1,len(arr)):
+        up=curr-1
+        if arr[curr]<arr[up]:
+            temp=arr[curr]
+            arr[curr]=arr[up]
+            up=up-1
+            while up>=0 and temp<arr[up]:
+                arr[up+1]=arr[up]
+                up=up-1
+            arr[up+1]=temp;
+    return arr;
+print(insert(arr1))
+
+
+
+
+
+
+
+
